05f7f50e7f185b606ad58d55804891552ab4f0fa
dssaharov <sugatokavary@gmail.com>
2026-05-13 16:54:48 +0200

    [make] add button after avg mark

--- handlers/menu.py
+++ handlers/menu.py
@@ -2,6 +2,8 @@ from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from aiogram import Router,F
 from aiogram.types import Message, CallbackQuery
+
+from keyboards.inline import change_marks_kb, choice_marks_kb
 from lexicons.lexicons_ru import MENU_TEXT_OUR,MENU_TEXT0, DEL_MARKS_TXT,ADD_MARKS_TEXT,DEL_MARKS_TXT_K,DEL_MARKS_TXT_K1,ADD_MARKS_TEXT1, DEL_MARKS_TXT_RES
 from math import ceil
 
@@ -37,17 +39,39 @@ async def process_marks(message: Message, state: FSMContext):
     """"Эта функция считатет средний балл и количество недостоющихся пятерок до желаемого балла"""
     print("[LOG] Считает средний балл и количество недостоющихся пятерок до желаемого балла")
     data = await state.get_data()
-    await state.update_data({"marks":message.text})
+    await state.update_data(marks = message.text)
     #await state.update_data(marks = data.get('marks','') + message.text)
 
     list_marks = list(map(int, list(message.text)))
     del_marks = sum(list_marks) / len(list_marks) # рассчет среднего балла
     add_sr = float(data.get('sr_add'))
-    k = len(list_marks) * (add_sr - del_marks) / (5-add_sr) # формула для подсчета недостоющихся пятерок до желаемого балла
 
-    await message.answer(f"{DEL_MARKS_TXT_RES}  {str(round(del_marks,2))} \n {DEL_MARKS_TXT_K(add_sr)}  {str(ceil(k))}  {DEL_MARKS_TXT_K1}")
+    k = len(list_marks) * (add_sr - del_marks) / ((5-add_sr) if add_sr < 5 else 1) # формула для подсчета недостоющихся пятерок до желаемого балла
+    kb = await change_marks_kb()
+    await message.answer(
+        f"{DEL_MARKS_TXT_RES}  {str(round(del_marks,2))} \n {DEL_MARKS_TXT_K(add_sr)}  {str(ceil(k))}  {DEL_MARKS_TXT_K1}",
+        reply_markup=kb
+    )
     print("[LOG] Вывел результат")
-    await state.clear() # очищает данные состояния
+    await state.set_state(None) # очищает данные состояния
 
 
+@router.callback_query(F.data == "change")
+async def add_marks(callback: CallbackQuery, state: FSMContext):
+
+    await callback.message.answer(
+        "Выберите оценку на которую вы исправите все двойки (только одно число)",
+        reply_markup=await choice_marks_kb()
+    )
+
+@router.callback_query(F.data.startswith("choice"))
+async def add_marks(callback: CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    marks = data.get("marks","")
+    new_mark = callback.data.split("_")[-1]
+    print(new_mark,marks,marks.replace("2",new_mark))
+    new_text = marks.replace("2",new_mark)
+    fake_message = callback.message.model_copy(update={"text": new_text})
+
+    await process_marks(fake_message, state)
 
--- keyboards/inline.py
+++ keyboards/inline.py
@@ -30,3 +30,37 @@ async def inline_progmarks_kb():
         resize_keyboard=True
     )
     return keyboards
+
+async def change_marks_kb():
+
+    button = InlineKeyboardButton(
+        text="исправить оценки",
+        callback_data="change"
+    )
+
+    keyboards = InlineKeyboardMarkup(
+        inline_keyboard=[[button]],
+        resize_keyboard=True
+    )
+    return keyboards
+
+
+async def choice_marks_kb():
+    data = {
+        "3":"choice_3",
+        "4":"choice_4",
+        "5":"choice_5"
+    }
+    s = []
+    for key, value in data.items():
+        button = InlineKeyboardButton(
+            text=key,
+            callback_data=value
+        )
+        s.append([button])
+
+    keyboards = InlineKeyboardMarkup(
+        inline_keyboard=s,
+        resize_keyboard=True
+    )
+    return keyboards
